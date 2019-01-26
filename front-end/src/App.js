48e375db1076e04c288d9603579636481999f7f5
philbeaudry <philippe.beaudry96@gmail.com>
2019-01-26 18:02:26 -0500

    Simple marker start

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,26 +1,38 @@
 import React, { Component } from 'react';
-import { Map, GoogleApiWrapper } from 'google-maps-react';
+import { Map, GoogleApiWrapper, Marker} from 'google-maps-react';
 
 const styles = require('./styles/GoogleMapStyles.json')
 
 export class MapContainer extends Component {
+
+  addMarker = (location, map) => {
+    console.log(location);
+  }
+
   render() {
     return (
-      <Map
-        google={this.props.google}
-        zoom={4}
-        styles={styles}
-        mapTypeControl={false}
-        streetViewControl={false}
-        panControl={false}
-        zoomControl={false}
-        rotateControl={false}
-        fullscreenControl={false}
-        initialCenter={{
-         lat: 41.850033,
-         lng: -87.6500523
-        }}
-      />
+      <body>
+        <h1>ConUHack</h1>
+        <p>Gab, Phil & Feken</p>
+        <Map
+          google={this.props.google}
+          zoom={4}
+          styles={styles}
+          mapTypeControl={false}
+          streetViewControl={false}
+          panControl={false}
+          zoomControl={false}
+          rotateControl={false}
+          fullscreenControl={false}
+          initialCenter={{
+            lat: 41.850033,
+            lng: -87.6500523
+          }}
+          onClick={(t, map, c) => this.addMarker(c.latLng, map)}
+        >
+        <Marker lat={41.850033} lng={-87.6500523} />  
+        </Map>
+      </body>
     );
   }
 }
