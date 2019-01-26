b99d4530f6fd45169348f09717810d8acaed18ed
veken1199 <super_veko@yahoo.com>
2019-01-26 17:11:01 -0500

    Added simple route to accept an array of coordinates and start_date, end_date

--- backend/api/plays.py
+++ backend/api/plays.py
@@ -1,9 +1,14 @@
-from flask import Blueprint, jsonify, session
+from flask import Blueprint, jsonify, session, request
+from flask_apispec import use_kwargs
+from schema import plays
 
 plays_blueprint_name = 'plays'
 plays_route = Blueprint(plays_blueprint_name, __name__, url_prefix='/{}'.format(plays_blueprint_name))
 
 
-@plays_route.route('', methods=['GET'])
+@plays_route.route('', methods=['POST'])
 def reset_data():
-    return jsonify({'data':'Hello world'})
+    request_data, err = plays.PlaysRequestSchema().load(request.get_json(force=True))
+    if err:
+        return jsonify({'data':{}, 'error': err})
+    return jsonify({'data': request_data})
