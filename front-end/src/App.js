d0b2dabd9214c1207ca3c832a90bd94eff7fa785
philbeaudry <philippe.beaudry96@gmail.com>
2019-01-26 15:24:53 -0500

    Added style to the map

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -3,18 +3,19 @@ import { Map, GoogleApiWrapper } from 'google-maps-react';
 
 const styles = require('./styles/GoogleMapStyles.json')
 
-const mapStyles = {
-  width: '100%',
-  height: '100%'
-};
-
 export class MapContainer extends Component {
   render() {
     return (
       <Map
         google={this.props.google}
         zoom={4}
-        style={styles}
+        styles={styles}
+        mapTypeControl={false}
+        streetViewControl={false}
+        panControl={false}
+        zoomControl={false}
+        rotateControl={false}
+        fullscreenControl={false}
         initialCenter={{
          lat: 41.850033,
          lng: -87.6500523
