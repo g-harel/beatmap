d20623316337830276389ba59b93e9335fe188b1
veken1199 <super_veko@yahoo.com>
2019-01-27 08:46:55 -0500

    Fixed unknonw data

--- backend/helpers/request_helper.py
+++ backend/helpers/request_helper.py
@@ -1,4 +1,10 @@
 import requests
 
 def get(url):
-    return requests.get(url=url,headers={"client-secret":"9923ac9b-8fd3-421f-b0e5-952f807c6885"}).json()
+    resp = requests.get(url=url,headers={"client-secret":"9923ac9b-8fd3-421f-b0e5-952f807c6885"})
+    print(resp)
+    if resp.status_code == 500:
+        print("herre")
+        return None
+    else:
+        return resp.json()
