446bcfbe9231d06fe87186d94934d1e45b4f8ca9
Gabriel <gabrielj.harel@gmail.com>
2019-01-27 08:07:47 -0500

    add loading state

--- frontend/sidebar.ts
+++ frontend/sidebar.ts
@@ -1,15 +1,29 @@
 import {fetchData, stripes} from "./utils";
-import {MapData} from "./types";
+import {MapData, ApiData} from "./types";
 
 const component = document.getElementById("sidebar");
 if (!component) throw "Could find sidebar";
 
-export const updateSidebar = async (coordinates: MapData | null): Promise<any> => {
+const drawWaiting = () => {
     const background = stripes({color: "#2a2a2a"});
     component.innerHTML = `<div class="waiting" style="${background}"></div>`;
-    if (!coordinates) return;
+};
 
-    const {data} = await fetchData(coordinates);
+const drawLoading = () => {
+    let html = "";
+    for (let i = 0; i < 10; i++) {
+        html += `
+            <div class="item loading">
+                <div class="count loading">&nbsp;</div>
+                <div class="song loading">&nbsp;</div>
+                <div class="artist loading">&nbsp;</div>
+            </div>
+        `;
+    }
+    component.innerHTML = html;
+};
+
+const drawReady = ({data}: ApiData) => {
     let html = "";
     data.forEach((item, i) => {
         html += `
@@ -21,6 +35,13 @@ export const updateSidebar = async (coordinates: MapData | null): Promise<any> =
         `;
     });
     component.innerHTML = html;
+}
+
+export const updateSidebar = async (coordinates: MapData | null): Promise<any> => {
+    drawWaiting();
+    if (!coordinates) return;
+    drawLoading();
+    drawReady(await fetchData(coordinates));
 };
 
 updateSidebar(null);
